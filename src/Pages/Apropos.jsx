7e87559ee90583a121512940eb1dc7a218eedcea
NELSON PARAISO <paraisonelson@gmail.com>
2025-01-22 11:40:49 +0100

    new push

--- src/Pages/Apropos.jsx
+++ src/Pages/Apropos.jsx
@@ -101,7 +101,7 @@ const Apropos = () => {
   const playAudio = () => {
     const audio = document.getElementById("background-audio");
     if (audio) {
-      audio.volume = 0.03; // Appliquer le volume réduit
+      audio.volume = 0.02; // Appliquer le volume réduit
       const playPromise = audio.play();
       if (playPromise !== undefined) {
         playPromise
