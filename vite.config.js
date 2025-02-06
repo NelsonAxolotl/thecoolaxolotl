a9ad624edbe33faab01696b378f33e00b46b1585
NELSON PARAISO <paraisonelson@gmail.com>
2025-02-06 21:43:19 +0100

    qsdfghjklm

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import compression from "vite-plugin-compression";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), compression()],
-  build: {
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-      output: {
-        comments: false,
-      },
-    },
-  },
+  plugins: [react()],
 });
