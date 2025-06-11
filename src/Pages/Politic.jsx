3f60fd561dac2d6325a9bbbb30988b9d622bcdf1
NELSON PARAISO <paraisonelson@gmail.com>
2025-06-11 21:23:07 +0200

    return

--- src/Pages/Politic.jsx
+++ src/Pages/Politic.jsx
@@ -8,66 +8,70 @@ import End from "../Components/End";
 
 const Politic = () => {
   const { t } = useTranslation();
+  return (
+    <>
+      <Helmet>
+        <title>{t("privacy.metaTitle")}</title>
+        <meta name="description" content={t("privacy.metaDescription")} />
+        <meta property="og:title" content={t("privacy.metaTitle")} />
+        <meta
+          property="og:description"
+          content={t("privacy.metaDescription")}
+        />
+        <meta
+          property="og:image"
+          content="https://thecoolaxolotl.com/Picspolitiqueaxo.webp"
+        />
+      </Helmet>
 
-  <>
-    <Helmet>
-      <title>{t("privacy.metaTitle")}</title>
-      <meta name="description" content={t("privacy.metaDescription")} />
-      <meta property="og:title" content={t("privacy.metaTitle")} />
-      <meta property="og:description" content={t("privacy.metaDescription")} />
-      <meta
-        property="og:image"
-        content="https://thecoolaxolotl.com/Picspolitiqueaxo.webp"
-      />
-    </Helmet>
+      <Nav />
+      <div className="politic">
+        <img
+          src="Pics/politique.webp"
+          alt="axolotl politic"
+          width="200"
+          height="200"
+          className="round-image-politic"
+          fetchpriority="high"
+        />
+        <div className="container-politic">
+          <h1>{t("privacy.title")}</h1>
 
-    <Nav />
-    <div className="politic ">
-      <img
-        src="Pics/politique.webp"
-        alt="axolotl politic"
-        width="200"
-        height="200"
-        className="round-image-politic"
-        fetchpriority="high"
-      />
-      <div className="container-politic">
-        <h1>{t("privacy.title")}</h1>
+          {[...Array(12)].map((_, i) => {
+            const sectionKey = `privacy.section${i + 1}`;
+            const sectionId = i + 1;
 
-        {[...Array(12)].map((_, i) => {
-          const sectionKey = `privacy.section${i + 1}`;
-          const sectionId = i + 1;
-
-          return (
-            <section key={i} id={`section-${i + 1}`}>
-              <h2>{t(`${sectionKey}.title`)}</h2>
-              {i + 1 === 12 ? (
-                <p>
-                  <Trans
-                    i18nKey={`${sectionKey}.content`}
-                    components={{
-                      a: (
-                        <a
-                          href="mailto:thecoolaxolotldesigner@gmail.com"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="email-link"
-                        />
-                      ),
-                    }}
-                    values={{ email: "thecoolaxolotldesigner@gmail.com" }}
-                  />
-                </p>
-              ) : (
-                <p>{t(`${sectionKey}.content`)}</p>
-              )}
-            </section>
-          );
-        })}
+            return (
+              <section key={i} id={`section-${i + 1}`}>
+                <h2>{t(`${sectionKey}.title`)}</h2>
+                {i + 1 === 12 ? (
+                  <p>
+                    <Trans
+                      i18nKey={`${sectionKey}.content`}
+                      components={{
+                        a: (
+                          <a
+                            href="mailto:thecoolaxolotldesigner@gmail.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="email-link"
+                          />
+                        ),
+                      }}
+                      values={{ email: "thecoolaxolotldesigner@gmail.com" }}
+                    />
+                  </p>
+                ) : (
+                  <p>{t(`${sectionKey}.content`)}</p>
+                )}
+              </section>
+            );
+          })}
+        </div>
       </div>
-    </div>
-    <End />
-  </>;
+      <End />
+    </>
+  );
 };
 
 export default Politic;
