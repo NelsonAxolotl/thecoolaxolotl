2e32443360c4b0387d3da48f94e18b29e7aa55a8
NELSON PARAISO <paraisonelson@gmail.com>
2025-07-16 11:24:21 +0200

    index

--- src/Components/TimelineSection.jsx
+++ src/Components/TimelineSection.jsx
@@ -5,38 +5,45 @@ import hand from "../Pics/client.webp";
 import analyse from "../Pics/analyse.webp";
 import maintenance from "../Pics/maintenance.webp";
 import int from "../Pics/prototype2.webp";
+import defaultImage from "../Pics/client.webp"; // image fallback sûre
 
 const TimelineSection = ({ steps }) => {
   const { t } = useTranslation();
 
   const images = [hand, analyse, proto, int, dev, maintenance];
-  const defaultImage = hand; // Ou une autre image par défaut
+
   return (
     <div className="timeline-container2">
       <h2 className="timeline-title2">{t("timeline.title")}</h2>
       <div className="timeline2">
-        {steps.map((step, index) => (
-          <div
-            className={`timeline-item2 ${
-              index % 2 === 0 ? "left" : "right"
-            } hidden`}
-            key={index}
-          >
-            <span className="timeline-number2">{index + 1}</span>
-            <div className="timeline-content2">
-              <h3 className="timeline-step2">{step.title}</h3>
-              <p className="timeline-description2">{step.description}</p>
+        {steps.map((step, index) => {
+          // Récupérer l'image avec fallback
+          const imgSrc =
+            typeof images[index] === "string" ? images[index] : defaultImage;
+
+          return (
+            <div
+              className={`timeline-item2 ${
+                index % 2 === 0 ? "left" : "right"
+              } hidden`}
+              key={index}
+            >
+              <span className="timeline-number2">{index + 1}</span>
+              <div className="timeline-content2">
+                <h3 className="timeline-step2">{step.title}</h3>
+                <p className="timeline-description2">{step.description}</p>
+              </div>
+              <div className="timeline-image2">
+                <img
+                  src={imgSrc}
+                  alt={`Step ${index + 1}`}
+                  width={200}
+                  height={200}
+                />
+              </div>
             </div>
-            <div className="timeline-image2">
-              <img
-                src={images[index] || defaultImage}
-                alt={`Step ${index + 1}`}
-                width={200}
-                height={200}
-              />
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
